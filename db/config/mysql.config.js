8bf11fde17a4177313fa8636bddcb0f5c7834e0f
alpha.giel@beroe-inc.com <alpha.giel@beroe-inc.com>
2022-05-05 22:44:54 -0400

    test

--- db/config/mysql.config.js
+++ db/config/mysql.config.js
@@ -1,8 +1,15 @@
+// module.exports = { 
+//   HOST: "us-cdbr-east-05.cleardb.net",
+//   USER: "b24ded93e7567d",
+//   PASSWORD: "76b468d4",
+//   DB: "heroku_e3820264fa0117d"
+// }
+
 module.exports = { 
-  HOST: "us-cdbr-east-05.cleardb.net",
-  USER: "b24ded93e7567d",
-  PASSWORD: "76b468d4",
-  DB: "heroku_e3820264fa0117d"
+  HOST: process.env.HOST,
+  USER: process.env.USER,
+  PASSWORD: process.env.PASSWORD,
+  DB:process.env.DB
 }
 
 // CLEARDB_DATABASE_URL => mysql://b24ded93e7567d:76b468d4@us-cdbr-east-05.cleardb.net/heroku_e3820264fa0117d?reconnect=true
